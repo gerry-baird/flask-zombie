8c8b09e9c1f09efa313fd32e0482de600308d738
gerry <gerry.baird@uk.ibm.com>
2023-07-27 14:55:46 +0100

    initial

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.get('/zombie')
+def hello_world():  # put application's code here
+    return {"risk": "42"}
+
+if __name__ == '__main__':
+    app.run()
